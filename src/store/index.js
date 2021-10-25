4c0fb266995c4e4fd246ee0704276450de1c4ef3
Guilherme Serpa Batista <serpa419@gmail.com>
2021-10-25 16:05:18 -0300

    first build

---
+++ src/store/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    tarefas: [
+      {
+        id: 1,
+        titulo: "Ir ao mercado",
+        concluido: false,
+      },
+      {
+        id: 2,
+        titulo: "Comprar Ração",
+        concluido: true,
+      },
+    ],
+  },
+  mutations: {
+    adicionaTarefa(state, titulo) {
+      if (titulo) {
+        state.tarefas.push({
+          id: new Date().getTime(),
+          titulo,
+          concluido: false,
+        });
+        this.campoInput = null;
+      }
+    },
+    removeTarefa(state, id) {
+      state.tarefas = state.tarefas.filter(tarefa => tarefa.id !== id)
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
